ffb2e581b25a789228b9ecbae544e5d036432dec
rilian <dmitriyis@gmail.com>
2012-07-31 00:14:52 +0300

    initially added rails 3.2.7

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails4LiveStreaming::Application.config.secret_token = '7f987ec8227b1bc0b52342df1289f7dc5f0272771dce11619906637c3d4b8338f887ed32c04fd03af17ae0d3401f501a85bc435fda5eb2a9162a70cd0f585070'
